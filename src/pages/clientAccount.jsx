773928b186d47b50f8bac0729450bdbd469f1804
Morgane Dassonville <morgane.dassonville@eemi.com>
2024-06-14 15:57:43 +0200

    add account pages

---
+++ src/pages/clientAccount.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ClientAccount = () => {
+  return (
+    <div>
+      <h1>Espace client public</h1>
+    </div>
+  );
+};
+
+export default ClientAccount;
---
+++ src/pages/corporateAccount.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CorporateAccount = () => {
+  return (
+    <div>
+      <h1>Espace entreprise</h1>
+    </div>
+  );
+};
+
+export default CorporateAccount;
