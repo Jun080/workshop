359238e2d06fa7d2164295da4728bfce1e85d502
Morgane Dassonville <morgane.dassonville@eemi.com>
2024-07-01 20:52:26 +0200

    add variable environnement

--- server.js
+++ server.js
@@ -4,21 +4,22 @@ const mysql = require('mysql');
 const cors = require('cors');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+require('dotenv').config(); // Charge les variables d'environnement depuis un fichier .env
 
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001; // Utilise le port défini dans les variables d'environnement ou 3001 par défaut
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-const SECRET_KEY = 'votre_secret_key';
+const SECRET_KEY = process.env.SECRET_KEY; // Utilise la clé secrète définie dans les variables d'environnement
 
 app.use(cors());
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'asihXcPDyR2PHXd',
-    database: 'workshopjo'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
 db.connect(err => {
@@ -26,9 +27,10 @@ db.connect(err => {
         console.error('Error connecting to the database:', err);
         return;
     }
+    console.log('Connected to the database');
 });
 
-// pour ajouter les données des clients dans la table users
+// Route pour ajouter les données des clients dans la table users
 app.post('/api/users', async (req, res) => {
     const { nom, prénom, email, password } = req.body;
     const hashedPassword = await bcrypt.hash(password, 10);
@@ -44,7 +46,7 @@ app.post('/api/users', async (req, res) => {
     });    
 });
 
-// pour ajouter les données des entreprises dans la table corporate_clients
+// Route pour ajouter les données des entreprises dans la table corporate_clients
 app.post('/api/corporate_clients', async (req, res) => {
     const { company_name, company_email, company_password } = req.body;
     const hashedPassword = await bcrypt.hash(company_password, 10);
@@ -90,8 +92,20 @@ app.post('/api/login', (req, res) => {
     });
 });
 
+// Route pour inscrire un utilisateur à un tournoi
+app.post('/api/register', (req, res) => {
+    const { userId, tournamentId } = req.body;
 
-
+    const sql = 'INSERT INTO registrations (user_id, tournament_id) VALUES (?, ?)';
+    db.query(sql, [userId, tournamentId], (err, result) => {
+        if (err) {
+            console.error('Erreur lors de l\'inscription :', err);
+            res.status(500).json({ error: 'Erreur lors de l\'inscription' });
+            return;
+        }
+        res.status(200).json({ message: 'Inscription réussie' });
+    });
+});
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}/`);
