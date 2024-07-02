99557bf4ec568424a9b3c0d6c64ad691374f7362
Morgane Dassonville <morgane.dassonville@eemi.com>
2024-07-02 14:25:23 +0200

    update localhost

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
-const port =  3001;
+const port = process.env.PORT || 3001;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
